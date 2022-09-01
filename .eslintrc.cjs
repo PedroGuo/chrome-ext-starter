3a35eb6fb3990146f858ee5aada233d37953fca4
guoyan <guoyan@exexm.com>
2022-09-01 12:29:53 +0800

    feat: change eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,12 +7,14 @@ module.exports = {
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
   ],
   parserOptions: {
     ecmaVersion: "latest",
   },
+  globals: {
+    "chrome": 'readonly'
+  },
   rules: {
-    "vue/multi-word-component-names": 0,
+    "vue/multi-word-component-names": 0
   },
 };
